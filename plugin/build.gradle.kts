e77df1f405a05a07419eb10e86682c1663aef90c
Qekly <benjaalejandro206@gmail.com>
2024-02-06 00:27:16 -0300

    chore: Set shadowJar options and modify options.release.set option for compileJava task

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -4,18 +4,30 @@ plugins {
     alias(libs.plugins.blossom)
 }
 
+val group = property("group") as String
 val version = property("version") as String
 
 tasks {
+    build {
+        dependsOn(shadowJar)
+    }
     compileJava {
         options.encoding = Charsets.UTF_8.name()
-        options.release.set = 17
+        options.release.set(17)
     }
     clean {
         delete("bin")
     }
     shadowJar {
+        archiveBaseName.set(rootProject.name)
+        archiveClassifier.set("")
 
+        destinationDirectory.set(file("$rootDir/bin/"))
+        minimize()
+
+        relocate("dev.triumphteam.cmd", "$group.libs.dev.triumphteam.cmd")
+        relocate("org.spongepowered", "$group.libs.org.spongepowered")
+        relocate("io.leangen.geantyref", "$group.libs.io.leangen.geantyref")
     }
 }
 
